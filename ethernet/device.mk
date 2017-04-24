135dc7f29e19a3027f298313bf0a43a9adac65fd
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2017-04-24 14:44:53 +0200

    HACK: Fix ethernet.rc

--- ethernet/device.mk
+++ ethernet/device.mk
@@ -1,4 +1,4 @@
-PRODUCT_PACKAGES := ethernet.rc
+PRODUCT_PACKAGES := ethernet.rc dhcpcd-6.8.2
 
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
